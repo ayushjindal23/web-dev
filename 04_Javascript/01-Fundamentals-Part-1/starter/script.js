20bf2bf315bb6cf87280834f3fcaad712dd9fec2
Ayush Jindal <64908612+ayushjindal23@users.noreply.github.com>
2023-08-26 05:24:41 -0700

    Add files via upload

---
+++ 04_Javascript/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,48 @@
+// let js ='amazing';
+// if(js=='amazing') console.log ('HI ayush');
+
+// console.log(40+-6*7);
+// console.log("2");
+
+// let xx = 'aj';
+// console.log(xx);
+
+// console.log(true);
+
+// let ayush = true;
+// console.log(ayush);
+
+// console.log(typeof true);
+// console.log(typeof ayush);
+// console.log(typeof 23);
+// console.log(typeof 'ayuish');
+
+// ayush = 23;
+
+// console.log(typeof ayush);
+
+// const firstName = 'aj';
+// const job = 'jobless';
+// const birth = 2000;
+// const year = 2023;
+
+// const ayush = "I'm " + firstName + ', a ' + (year - birth) + ' year old ' + job;
+
+// console.log(ayush);
+
+// const anew =  `I'm ${firstName}, a ${year-birth} year old ${job}`;
+// console.log(anew);
+
+// console.log(`ayush
+// jonfal
+// daljla`);
+
+const age = '18';
+if (age === 18) console.log('You just became an adult :D (strict)');
+
+if (age == 18) console.log('You just became an adult :D (loose)');
+
+const favourite = Number(prompt("What's your favourite number?"));
+console.log(favourite);
+console.log(typeof favourite);
+
